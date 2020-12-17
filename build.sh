549e8de832c771eb70cf7bbd145d5280b1d8db80
mob-sakai <sakai861104@gmail.com>
2020-12-17 10:51:02 +0900

    feat: add local build script

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash -ex
+
+source .github/workflows/.env
+
+UNITY_VERSION=${1:-2021.1.0a7}
+MODULE=${2:-base}
+CHANGESET=`unity-changeset $UNITY_VERSION`
+
+REPO_LATEST_TAG=`git tag --sort='v:refname' \
+            | tail -n 1 \
+            | cut -d '/' -f 3`
+
+ARGUMETNS="$(echo \
+    --build-arg hubImage=$HUB_IMAGE:$REPO_LATEST_TAG \
+    --build-arg baseImage=$BASE_IMAGE:$REPO_LATEST_TAG \
+    --build-arg version=$UNITY_VERSION \
+    --build-arg changeSet=$CHANGESET \
+    --build-arg module=$MODULE \
+    --tag $EDITOR_IMAGE:$UNITY_VERSION-$MODULE-dev \
+    --tag $EDITOR_IMAGE:$UNITY_VERSION-$MODULE-$REPO_LATEST_TAG-dev \
+)"
+
+if [ "$MODULE" = 'base'] ; then
+    docker build -f .github/workflows/dockerfiles/editor.Dockerfile $ARGUMETNS .
+else
+    docker build -f .github/workflows/dockerfiles/editor_module.Dockerfile $ARGUMETNS .
+fi
